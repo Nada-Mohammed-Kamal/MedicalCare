594ef3620316fa521cd90083e9e8b1290355cf48
Esraa Sayed <esraasayedahmed1999@gmail.com>
2022-04-20 21:08:45 +0200

    finish show med successfully

--- app/src/main/java/com/example/medicalappreminder_java/HomeScreen/Presenter/AllMedPresenter.java
+++ app/src/main/java/com/example/medicalappreminder_java/HomeScreen/Presenter/AllMedPresenter.java
@@ -1,8 +1,16 @@
 package com.example.medicalappreminder_java.HomeScreen.Presenter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import com.example.medicalappreminder_java.HomeScreen.View.HomeFragment.AllMedViewInterface;
+import com.example.medicalappreminder_java.Repo.RepoClass;
+import com.example.medicalappreminder_java.Repo.local.ConcreteLocalSource;
+import com.example.medicalappreminder_java.Repo.local.LocalSourceInterface;
+import com.example.medicalappreminder_java.Repo.remote.FirestoreManger;
+import com.example.medicalappreminder_java.Repo.remote.RemoteSourceInterface;
+import com.example.medicalappreminder_java.models.Medicine;
+import com.example.medicalappreminder_java.models.User;
 import com.example.medicalappreminder_java.roomdb.AppDatabase;
 
 import java.util.List;
@@ -20,7 +28,13 @@ public class AllMedPresenter implements AllMedPresenterInterface{
     @Override
     public void getMeds() {
        // repositoryInterface.getAllMovies(this);
-       List med = AppDatabase.getDBInstance(_context).medicineDao().getAllMedicines();
-        _view.showData(med);
+        RemoteSourceInterface remoteSourceInterface = new FirestoreManger();
+        LocalSourceInterface localSourceInterface = new ConcreteLocalSource(_context);
+        RepoClass repoClass = RepoClass.getInstance(remoteSourceInterface,localSourceInterface,_context);
+        SharedPreferences preferences = _context.getSharedPreferences("preferencesFile" , Context.MODE_PRIVATE) ;
+        String userEmail = preferences.getString("emailKey" , "user email") ;
+        User currentUser = repoClass.findUserByEmail(userEmail);
+        List<Medicine> listOfMedications = currentUser.getListOfMedications();
+        _view.showData(listOfMedications);
     }
 }
